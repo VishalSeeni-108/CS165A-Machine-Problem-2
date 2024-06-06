33919edd12c59a7e0793fb24c27f59d74f91c51a
Vishal S <vishalseeni@gmail.com>
2024-06-06 04:12:34 -0700

    Full rewrite - minimax properly implemented

--- player1.py
+++ player1.py
@@ -1,170 +1,156 @@
-import random
+import math
 
 # Directions are mapped to 'W', 'A', 'S', 'D'
 directions = {'W': (0, -1), 'A': (-1, 0), 'S': (0, 1), 'D': (1, 0)}
-RECURSION_LIMIT = 10
-#TODO: Implement alpha-beta pruning so recursion limit can be increased - already very slow for just 10
+DEPTH_LIMIT = 5
 
 def player1_logic(coins, potions, foods, dungeon_map, self_position, other_agent_position):
-    #Get player position
-    x, y = self_position
+  
+    initial_state = {
+        "player_position": self_position,
+        "player_score": 0,
+        "player_stamina": 50, 
+        "player_hunger": 50, 
+        "player_hunger_dec": True,
+        "opponent_position": other_agent_position, 
+        "opponent_score": 0, 
+        "opponent_stamina": 50, 
+        "opponent_hunger": 50,
+        "opponent_hunger_dec": True,
+        "coins": coins.copy(),
+        "potions": potions.copy(),
+        "foods": foods.copy(),
+        "valid": True
+    }
 
-    # List to hold possible moves
-    valid_moves = []
-    
-    # Check each possible direction
+    potential_moves = {}
     for move in directions:
-        dx, dy = directions[move]
-        nx, ny = x + dx, y + dy
-        
-        # Ensure the new position is within the bounds of the map and is not a wall
-        # Notice that you have to access position (x, y) via dungeon_map[y][x]
-        # Because the dungeon_map is a list of lists
-        if dungeon_map[ny][nx] == 'floor':
-            valid_moves.append(move)
+            move_state = state_gen(initial_state, move, True, dungeon_map)
+            potential_moves[move] = recursive_minimax(move_state, False, 1, -math.inf, math.inf, dungeon_map)
+    print("Taking move " + max(potential_moves, key=potential_moves.get))
+    print(potential_moves)
+    return max(potential_moves, key=potential_moves.get)
+
+def utility(state): #for a given state, calculate a score based on player score, stamina, hunger and opponent score, stamina, hunger
+    return state["player_score"] - state["opponent_score"] #TODO - need to find a good utility function
+
+
+def recursive_minimax(state, turn, depth, alpha, beta, dungeon_map): #Recursive function for minimax
     
-    results = {}
-
-    for move in valid_moves:
-        results[move] = recursive_minimax(dungeon_map, coins, potions, foods, self_position, 0, 50, 50, other_agent_position, 0, 50, 50, True, True, 'W', True, 0)
-   
-    max_score = max(results)
-    # chosen_moves = []
-
-    # for result in results: 
-    #     if result == max_score:
-    #         chosen_moves.append()
-    #print("Final scores: " + str(results))
-    #print("Taking action " + max(results, key=results.get))
-    return max(results, key=results.get)
-
-
-def action_utility(coins, potions, foods, player_position, player_score, player_stamina, player_hunger, opponent_position, opponent_score, opponent_stamina, opponent_hunger, action, hunger_dec_player, hunger_dec_opponent, turn): #Evaluates a given action and specifices the changed values - that is, calculate the resulting state  
+    if(depth == DEPTH_LIMIT):
+        return utility(state)
+    
     if(turn): #Player's turn
-        # Evaluates the results of a given action and returns appropriate values
-        #Calculate action_position
-        xi, yi = player_position
-        dx, dy = directions[action]
-        action_position = xi+dx, yi+dy
+        new_states = []
+        for move in directions:
+            new_states.append(state_gen(state, move, True, dungeon_map))
 
-        #Decrement stamina for movement
-        if(player_position != action_position): #Ensure this is not an idle action
-            player_stamina -= 1
-
-        #Decrement stamina for hunger
-        if(player_hunger == 0):
-            player_stamina -= 1
-
-        #Decrement hunger if hunger was not decremented on previous turn
-        if(not hunger_dec_player): #If hunger was not decremented on previous turn
-            hunger_dec_player = True
-            player_hunger -= 1
-        else: #Hunger was decremented on previous turn
-            hunger_dec_player = False
-
-        #Check for items 
-        #print("Action position: " + str(action_position))
-        if action_position in coins: #Check if there is a coin at that position - if there is, increment score and collect coin
-            player_score += 1
-            coins.remove(action_position) 
-        elif action_position in foods:  #Check if there is a food at that position - if there is, increment hunger appropriately 
-            player_hunger += 30
-            if(player_hunger > 50): 
-                player_hunger = 50
-            foods.remove(action_position)
-        elif action_position in potions: #Check if there is a potion at that position 0 if there is increment stamina appropriately
-            player_stamina += 20
-            if(player_stamina > 50):
-                player_stamina = 50
-            potions.remove(action_position)
+        curr_max = -math.inf
+        for new_state in new_states:
+            new_score = recursive_minimax(new_state, False, depth+1, alpha, beta, dungeon_map)
+            curr_max = max(curr_max, new_score)
+            alpha = max(alpha, curr_max)
+            if(beta <= alpha):
+                break
+        return curr_max
     else: #Opponent's turn
-        # Evaluates the results of a given action and returns appropriate values
-        #Calculate action_position
-        xi, yi = opponent_position
+        new_states = []
+        for move in directions:
+            new_states.append(state_gen(state, move, False, dungeon_map))
+
+        curr_min = math.inf
+        for new_state in new_states:
+            new_score = recursive_minimax(new_state, True, depth+1, alpha, beta, dungeon_map)
+            curr_min = min(curr_min, new_score)
+            beta = min(beta, curr_min)
+            if(beta <= alpha):
+                break
+        return curr_min
+
+
+
+def state_gen(state, action, player, dungeon_map):
+    new_state = state.copy()
+
+    if(player):
+        #Get player position
+        x, y = new_state["player_position"]
         dx, dy = directions[action]
-        action_position = xi+dx, yi+dy
-
-        #Decrement stamina for movement
-        if(opponent_position != action_position): #Ensure this is not an idle action
-            opponent_stamina -= 1
-
-        #Decrement stamina for hunger
-        if(opponent_hunger == 0):
-            opponent_stamina -= 1
-
-        #Decrement hunger if hunger was not decremented on previous turn
-        if(not hunger_dec_opponent): #If hunger was not decremented on previous turn
-            hunger_dec_opponent = True
-            opponent_hunger -= 1
-        else: #Hunger was decremented on previous turn
-            hunger_dec_opponent = False
-
-        #Check for items 
-        if action_position in coins: #Check if there is a coin at that position - if there is, increment score and collect coin
-            opponent_score += 1
-            coins.remove(action_position) 
-        elif action_position in foods:  #Check if there is a food at that position - if there is, increment hunger appropriately 
-            opponent_hunger += 30
-            if(opponent_hunger > 50): 
-                opponent_hunger = 50
-            foods.remove(action_position) 
-        elif action_position in potions: #Check if there is a potion at that position 0 if there is increment stamina appropriately
-            opponent_stamina += 20
-            if(opponent_stamina > 50):
-                opponent_stamina = 50
-            potions.remove(action_position) 
-        
-    return coins, potions, foods, player_score, player_stamina, player_hunger, hunger_dec_player, opponent_score, opponent_stamina, opponent_hunger, hunger_dec_opponent, action_position
-
-def utility(player_score, player_stamina, player_hunger, opponent_score, opponent_stamina, opponent_hunger): #for a given state, calculate a score based on player score, stamina, hunger and opponent score, stamina, hunger
-    ##return ((10*player_score) + (1*player_stamina) + (1*player_hunger) - (10*opponent_score) - (10*opponent_stamina) - (10*opponent_hunger))/6 #Returns weighted average
-    print("Player score: " + str(player_score))
-    print("Opponent score: " + str(opponent_score))
-    return player_score - opponent_score
-def recursive_minimax(dungeon_map, coins, potions, foods, player_position, player_score, player_stamina, player_hunger, opponent_position, opponent_score, opponent_stamina, opponent_hunger, hunger_dec_player, hunger_dec_opponent, curr_action, turn, recursion_depth): #Recursive function for minimax
-    #Calculate next moves for the either the player or the opponent taking their turn
-    # Get current position of the agent
-    if(turn): #Player's turn
-        x, y = player_position
-    else: #Opponent's turn
-        x, y = opponent_position
-    
-    # List to hold possible moves
-    valid_moves = []
-    
-    # Check each possible direction
-    for move in directions:
-        dx, dy = directions[move]
         nx, ny = x + dx, y + dy
-        
         # Ensure the new position is within the bounds of the map and is not a wall
-        # Notice that you have to access position (x, y) via dungeon_map[y][x]
-        # Because the dungeon_map is a list of lists
-        if dungeon_map[ny][nx] == 'floor':
-            valid_moves.append(move)
-    
-    # If there are no valid move, break
-    if((recursion_depth == RECURSION_LIMIT) or (len(valid_moves) == 0)):
-        return utility(player_score, player_stamina, player_hunger, opponent_score, opponent_stamina, opponent_hunger)
+            # Notice that you have to access position (x, y) via dungeon_map[y][x]
+            # Because the dungeon_map is a list of lists
+        if dungeon_map[ny][nx] != 'floor':
+            new_state["valid"] = False
+            return new_state
+        
+        new_state["player_position"] = nx, ny
+        
+        #Check for items
+        if(new_state["player_position"] in new_state["coins"]):
+            new_state["player_score"] += 1
+            new_state["coins"].remove(new_state["player_position"])
+        elif(new_state["player_position"] in new_state["potions"]):
+            new_state["player_stamina"] += 20
+            if(new_state["player_stamina"] > 50):
+                new_state["player_stamina"] = 50
+            new_state["potions"].remove(new_state["player_position"])
+        elif(new_state["player_position"] in new_state["foods"]):
+            new_state["player_hunger"] += 30
+            if(new_state["player_hunger"] > 50):
+                new_state["player_hunger"] = 50
+            new_state["foods"].remove(new_state["player_position"])
 
-    #For each possible move, calculate potential results and store recursive call
-    move_scores = {}
+        #Decrement hunger and stamina
+        if(new_state["player_hunger"] == 0):
+            new_state["player_stamina"] -= 2
+        else:
+            new_state["player_stamina"] -= 1
+        if(new_state["player_hunger_dec"]): #Decremented hunger on previous turn
+            new_state["player_hunger_dec"] = False
+        else: 
+            new_state["player_hunger_dec"] = True
+            new_state["player_hunger"] -= 1
+            new_state["player_stamina"] += 1
+    else: 
+        #Get opponent position
+        x, y = new_state["opponent_position"]
+        dx, dy = directions[action]
+        nx, ny = x + dx, y + dy
+        # Ensure the new position is within the bounds of the map and is not a wall
+            # Notice that you have to access position (x, y) via dungeon_map[y][x]
+            # Because the dungeon_map is a list of lists
+        if dungeon_map[ny][nx] != 'floor':
+            new_state["valid"] = False
+            return new_state
+        
+        new_state["opponent_position"] = nx, ny
+        
+        #Check for items
+        if(new_state["opponent_position"] in new_state["coins"]):
+            new_state["opponent_score"] += 1
+            new_state["coins"].remove(new_state["opponent_position"])
+        elif(new_state["opponent_position"] in new_state["potions"]):
+            new_state["opponent_stamina"] += 20
+            if(new_state["opponent_stamina"] > 50):
+                new_state["opponent_stamina"] = 50
+            new_state["potions"].remove(new_state["opponent_position"])
+        elif(new_state["opponent_position"] in new_state["foods"]):
+            new_state["opponent_hunger"] += 30
+            if(new_state["opponent_hunger"] > 50):
+                new_state["opponent_hunger"] = 50
+            new_state["foods"].remove(new_state["opponent_position"])
 
-    for action in valid_moves:
-        utility_coins = coins.copy()
-        utility_foods = foods.copy()
-        utility_potions = potions.copy()
-        action_coins, action_potions, action_foods, action_player_score, action_player_stamina, action_player_hunger, action_hunger_dec_player, action_opponent_score, action_opponent_stamina, action_opponent_hunger, action_hunger_dec_opponent, action_position = action_utility(utility_coins, utility_potions, utility_foods, player_position, player_score, player_stamina, player_hunger, opponent_position, opponent_score, opponent_stamina, opponent_hunger, action, hunger_dec_player, hunger_dec_opponent, not turn)
-        if(turn): #Player's turn
-            move_scores[action] = recursive_minimax(dungeon_map, action_coins, action_potions, action_foods, action_position, action_player_score, action_player_stamina, action_player_hunger, opponent_position, action_opponent_score, action_opponent_stamina, action_opponent_hunger, action_hunger_dec_player, action_hunger_dec_opponent, action, not turn, recursion_depth + 1)
-        else: #Opponent's turn
-            move_scores[action] = recursive_minimax(dungeon_map, action_coins, action_potions, action_foods, player_position, action_player_score, action_player_stamina, action_player_hunger, action_position, action_opponent_score, action_opponent_stamina, action_opponent_hunger, action_hunger_dec_player, action_hunger_dec_opponent, action, not turn, recursion_depth + 1)
+        #Decrement hunger and stamina
+        if(new_state["opponent_hunger"] == 0):
+            new_state["opponent_stamina"] -= 2
+        else:
+            new_state["opponent_stamina"] -= 1
+        if(new_state["opponent_hunger_dec"]): #Decremented hunger on previous turn
+            new_state["opponent_hunger_dec"] = False
+        else: 
+            new_state["opponent_hunger_dec"] = True
+            new_state["opponent_hunger"] -= 1
+            new_state["opponent_stamina"] += 1
 
-    #print(move_scores)
-
-    if(turn): #Player's turn, want to MAXIMIZE heuristic
-        #print("Max is " + str(max(move_scores.values())))
-        return max(move_scores.values())
-    else: #Opponent's turn, want to MINIMIZE heuristic
-        #print("Min is " + str(min(move_scores.values())))
-        return min(move_scores.values())
+    return new_state
